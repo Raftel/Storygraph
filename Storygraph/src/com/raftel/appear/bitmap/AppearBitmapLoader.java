2b90961764d1db4493bcb3a60c9f2cd1eef9bed2
Gooson Bazzinga <dwizard@naver.com>
2013-01-01 20:48:15 +0900

    AppearBitmapLoader 변경
    Model을 사용하던 구조에서 새롭게 callback을 이용하는 구조로 변경
    
    project fommater에서 comment의 제한이 80으로 되있던걸 200으로 변경

--- Storygraph/src/com/raftel/appear/bitmap/AppearBitmapLoader.java
+++ Storygraph/src/com/raftel/appear/bitmap/AppearBitmapLoader.java
@@ -9,16 +9,19 @@ import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.support.v4.util.LruCache;
 
-import com.raftel.appear.graphics.AppearModel;
-
 public class AppearBitmapLoader {
 	private Context mContext;
 	private LruCache<String, Bitmap> mMemoryCache;
 
+	public interface Callback {
+		public void setTextureLoadTask(TextureLoadTask task);
+		public TextureLoadTask getTextureLoadTask();
+		public void onBitmapLoaded(Bitmap bitmap);
+	}
+	
 	public AppearBitmapLoader(Context context) {
 		mContext = context;
-		// Get memory class of this device, exceeding this amount will throw an
-		// OutOfMemory exception.
+		// Get memory class of this device, exceeding this amount will throw an OutOfMemory exception.
 		final int memClass = ((ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE)).getMemoryClass();
 
 		// Use 1/8th of the available memory for this memory cache.
@@ -26,8 +29,7 @@ public class AppearBitmapLoader {
 
 		mMemoryCache = new LruCache<String, Bitmap>(cacheSize) {
 			protected int sizeOf(String key, Bitmap bitmap) {
-				// The cache size will be measured in bytes rather than number
-				// of items.
+				// The cache size will be measured in bytes rather than number of items.
 				return bitmap.getByteCount();
 			}
 		};
@@ -66,26 +68,20 @@ public class AppearBitmapLoader {
 		}
 	}
 
-	public void loadBitmap(final AppearBitmapData data, final AppearModel model, final int reqW, final int reqH, final boolean doCache) {
+	public void loadBitmap(final AppearBitmapData data, final AppearBitmapLoader.Callback callback, final int reqW, final int reqH, final boolean doCache) {
 
 		final String imageKey = getCacheKey(data);
-
 		Bitmap bitmap = getBitmapFromMemCache(imageKey);
 
 		if (bitmap != null) {
-			model.setTexture(bitmap);
+			callback.onBitmapLoaded(bitmap);
 		} else {
 			Activity av = (Activity) mContext;
 			av.runOnUiThread(new Runnable() {
 				public void run() {
-					if (cancelPotentialWork(data, model)) {
-						final TextureLoadTask task = new TextureLoadTask(model, reqW, reqH, doCache);
-
-						// TODO : Check by gooson
-						// if (model instanceof AppearModel) {
-						// ((AppearModel)model).setTexLoaderTask(task);
-						// }
-
+					if (cancelPotentialWork(data, callback)) {
+						final TextureLoadTask task = new TextureLoadTask(callback, reqW, reqH, doCache);
+						callback.setTextureLoadTask(task);
 						task.execute(data);
 					}
 				}
@@ -93,15 +89,10 @@ public class AppearBitmapLoader {
 		}
 	}
 
-	public static boolean cancelPotentialWork(AppearBitmapData data, AppearModel model) {
-		final TextureLoadTask task = null;
+	public static boolean cancelPotentialWork(AppearBitmapData data, AppearBitmapLoader.Callback callback) {
+		final TextureLoadTask task = (TextureLoadTask)callback.getTextureLoadTask();
 
-		// TODO : Check by gooson
-		// if (model instanceof AppearModel) {
-		// task = ((AppearModel) model).getTexLoaderTask();
-		// }
-
-		if (task != null) {
+		if (task != null && task instanceof TextureLoadTask) {
 			final AppearBitmapData prevData = task.data;
 			if (prevData != data) {
 				// Cancel previous task
@@ -112,22 +103,20 @@ public class AppearBitmapLoader {
 			}
 		}
 
-		// No task associated with the ImageView, or an existing task was
-		// cancelled
+		// No task associated with the ImageView, or an existing task was cancelled
 		return true;
 	}
 
 	public class TextureLoadTask extends AsyncTask<AppearBitmapData, Void, Bitmap> {
-		private final WeakReference<AppearModel> mModel;
+		private final WeakReference<AppearBitmapLoader.Callback> mCallback;
 		private AppearBitmapData data = null;
 		private int mReqWidth;
 		private int mReqHeight;
 		private boolean mDoCache;
 
-		public TextureLoadTask(AppearModel model, int reqWidth, int reqHeight, boolean doCache) {
-			// Use a WeakReference to ensure the ImageView can be garbage
-			// collected
-			mModel = new WeakReference<AppearModel>(model);
+		public TextureLoadTask(AppearBitmapLoader.Callback callback, int reqWidth, int reqHeight, boolean doCache) {
+			// Use a WeakReference to ensure the ImageView can be garbage collected
+			mCallback = new WeakReference<AppearBitmapLoader.Callback>(callback);
 			mReqWidth = reqWidth;
 			mReqHeight = reqHeight;
 			mDoCache = doCache;
@@ -179,19 +168,11 @@ public class AppearBitmapLoader {
 				bitmap = null;
 			}
 
-			if (mModel != null && bitmap != null) {
-				final AppearModel model = mModel.get();
-				if (model != null) {
-
-					if (mDoCache == true)
-						model.setTexture(bitmap);
-					else
-						model.setTexture(bitmap, true);
-
-					// TODO : Check by gooson
-					// if (model instanceof CBGallery3DModel) {
-					// 		((CBGallery3DModel) model).setTexLoaderTask(null);
-					// }
+			if (mCallback != null && bitmap != null) {
+				final AppearBitmapLoader.Callback callback = mCallback.get();
+				if (callback != null) {
+					callback.onBitmapLoaded(bitmap);
+					callback.setTextureLoadTask(null);
 				}
 			}
 		}
