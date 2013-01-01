54c671b86d437df635920a31b41d7de48ebf0d52
Gooson Bazzinga <dwizard@naver.com>
2013-01-02 00:35:40 +0900

    AppearRsourceManager initial commit

---
+++ Storygraph/src/com/raftel/appear/resource/AppearResourceManager.java
@@ -0,0 +1,12 @@
+package com.raftel.appear.resource;
+
+import android.content.Context;
+
+public class AppearResourceManager {
+
+	Context mContext;
+	
+	public AppearResourceManager(Context context) {
+		mContext = context;
+	}
+}
