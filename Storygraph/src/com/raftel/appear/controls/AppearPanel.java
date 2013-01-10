7b10c5ee80a65d637e887d854c37ba11eaee39e7
gooson <juneseong.joe@samsung.com>
2013-01-10 14:43:21 +0900

    AppearPanel initial commit

---
+++ Storygraph/src/com/raftel/appear/controls/AppearPanel.java
@@ -0,0 +1,16 @@
+package com.raftel.appear.controls;
+
+public class AppearPanel extends AppearControl {
+
+	public AppearPanel() {
+		
+	}
+	
+	public void addItem() {
+		
+	}
+	
+	public void deleteItem() {
+		
+	}
+}
